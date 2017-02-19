b81c53603ac8f217832184581eeeb7c99511ee54
Ezequiel Bertran <enbertran@gmail.com>
2017-02-19 16:56:41 -0300

    sort correctly months still in period of last year

--- api/domain/asistencias.php
+++ api/domain/asistencias.php
@@ -4,14 +4,14 @@ include_once 'domain/mails.php';
 
 function getAsistencias($aceptadas) {
 
-	$query = "SELECT a.*, coalesce(c.nombre,cb.nombre) as club, DATE_FORMAT(a.fecha,'%d-%m-%Y') as fecha, coalesce(coalesce(concat(coalesce(s.apellido,''), ', ', s.nombre), u.usuario),'-') as informante
+	$query = "SELECT a.*, coalesce(c.nombre,cb.nombre) as club, DATE_FORMAT(a.fecha,'%d-%m-%Y') as fecha, coalesce(coalesce(concat(coalesce(s.apellido,''), ', ', s.nombre), u.usuario),'-') as informante, if(cast(mes as UNSIGNED) > 6, periodo, periodo + 1) as anio
 						FROM asistencias a
 						left join clubes c on c.nro = a.nrclub
 						left join clubes_borrados cb on cb.nro = a.nrclub
 						left join usuarios u on u.id = a.usuario_id
 						left join socios s on s.orden = u.nrori
 						WHERE a.aceptado = $aceptadas
-						ORDER BY a.periodo desc, a.mes desc,
+						ORDER BY anio desc, a.mes desc,
 										--  date(a.fecha) desc,
 										 c.nombre";
 
